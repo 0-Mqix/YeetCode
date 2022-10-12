498ddf1fbbcc56497d7767f0f46a883e7c3224ed
Mqix <maxvanmoorsel2@gmail.com>
2022-10-12 13:48:32 +0200

    working min heap

--- solutions/heap/heap_test.go
+++ solutions/heap/heap_test.go
@@ -1,7 +1,28 @@
 package heap
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestHeap(t *testing.T) {
+func TestMinHeapInt(t *testing.T) {
+	heap := New(make([]int, 0))
+	values := []int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}
 
+	// Push
+	for _, v := range values {
+		heap.Push(v)
+	}
+	expected := []int{4, 6, 5, 8, 7, 10, 13, 12, 11, 9}
+	if !reflect.DeepEqual(heap.data, expected) {
+		t.Errorf("Expected %v, got %v", expected, heap.data)
+	}
+
+	// Pop
+	expected = []int{4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	for _, v := range expected {
+		if val := heap.Pop(); val != v {
+			t.Errorf("Expected %v, got %v", val, v)
+		}
+	}
 }
